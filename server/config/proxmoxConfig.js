c9de5994c321a81dc1e71f9c8eaad81a182033ac
DaniMa02 <danielmiguela411@gmail.com>
2024-06-12 22:52:25 +0200

    asdasds

--- server/config/proxmoxConfig.js
+++ server/config/proxmoxConfig.js
@@ -2,7 +2,7 @@ import proxmoxApi from 'proxmox-api';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const proxmox = proxmoxApi({
-    host: '10.243.0.1',
+    host: '10.4.0.56',
     password: 'Admin1234.',
     tokenID: 'root@pam',
     port: 8006
