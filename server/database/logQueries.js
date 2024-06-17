801b92fa55c20dc3dfc214e66d926c19ffb9eef3
DaniMa02 <danielmiguela411@gmail.com>
2024-06-17 22:32:51 +0200

    asioudbas

--- server/database/logQueries.js
+++ server/database/logQueries.js
@@ -3,7 +3,7 @@ import { initialize } from '../config/mySqlConfig.js';
 export async function getLogsFromView() {
   try {
     const connection = await initialize();
-    const [rows] = await connection.execute('SELECT * FROM tu_vista_de_logs');
+    const [rows] = await connection.execute('SELECT * FROM vista_logs');
     await connection.end();
     return rows;
   } catch (error) {
